ed06e3b953e40a01626f3cd9d6c6745e38137f19
Rubrasum <josephbetbeze@gmail.com>
2023-05-08 23:44:53 -0500

    Updated the footer to use components

---
+++ resources/views/layout/footer/about.blade.php
@@ -0,0 +1,6 @@
+<div class="md:w-1/4">
+    <h4 class="text-2xl font-bold mb-4 text-white">About Me</h4>
+    <p class="leading-loose mb-4"
+    >As a software engineer and consultant, I specialize in complex focused SaaS development, offering customized solutions that drive business growth and success.</p>
+    <a href="#" class="hover:text-white">Read more</a>
+</div>
