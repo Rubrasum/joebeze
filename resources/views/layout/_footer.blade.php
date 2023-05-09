4969771ab0a55037b5786721946f4537df2406b5
Rubrasum <josephbetbeze@gmail.com>
2023-05-08 23:42:52 -0500

    Finished a decent footer

---
+++ resources/views/layout/_footer.blade.php
@@ -0,0 +1,46 @@
+<footer class="bg-gray-900 text-gray-500">
+    <div class="container mx-auto py-12 flex flex-col md:flex-row md:items-start justify-between">
+        <div class="md:w-1/4">
+            <h4 class="text-2xl font-bold mb-4 text-white">About Me</h4>
+            <p class="leading-loose mb-4"
+            >As a software engineer and consultant, I specialize in complex focused SaaS development, offering customized solutions that drive business growth and success.</p>
+            <a href="#" class="hover:text-white">Read more</a>
+        </div>
+        <div class="md:w-1/4 mt-8 md:mt-0 ml-8">
+            <h4 class="text-2xl font-bold mb-4 text-white">Contact</h4>
+            <ul class="list-none">
+                <li class="mb-2 flex items-center"><i class="fas fa-envelope mr-2"></i> <a href="mailto:josephbetbeze@gmail.com" class="hover:text-white">josephbetbeze@gmail.com</a></li>
+                <li class="mb-6 flex items-center"><i class="fas fa-phone mr-2"></i> <a href="tel:+19044772732" class="hover:text-white">+1 (904) 477-2732</a></li>
+                <li class="mb-2 flex items-start">
+                    <div class="mr-2"><i class="fas fa-map-marker-alt"></i></div>
+                    <div>
+                        <span>Joseph Betbeze</span>
+                        <br>
+                        <span>211 South Greenwood Avenue,</span>
+                        <br>
+                        <span>Tulsa, FL 74120</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="md:w-1/4 mt-8 md:mt-0 ">
+            <h4 class="text-2xl font-bold mb-4 text-white">Social</h4>
+            <ul class="list-none flex">
+                <li class="mr-4"><a href="https://twitter.com/joebetbeze" class="hover:text-white text-4xl"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="https://www.instagram.com/josephbetbeze/" class="hover:text-white text-4xl"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+        </div>
+        <div class="md:w-1/4 mt-8 md:mt-0 ">
+            <h4 class="text-2xl font-bold mb-4 text-white">Newsletter</h4>
+            <p class="leading-loose mb-4">Sign up to receive updates and news about our services.</p>
+            <form action="#" method="post" class="flex flex-col md:flex-row items-center">
+                <input type="email" name="email" placeholder="Enter your email" class="bg-gray-800 text-gray-300 px-4 py-2 rounded-t md:rounded-l-none md:rounded-r">
+                <button type="submit" class="bg-gray-700 text-white px-4 py-2 rounded-b md:rounded-r-none md:rounded-l hover:bg-gray-600 transition duration-300 ease-in-out">Subscribe</button>
+            </form>
+        </div>
+    </div>
+    <div class="bg-gray-800 text-center py-4">
+        <p>&copy; 2023 Joseph Betbeze. All rights reserved.</p>
+    </div>
+</footer>
+
