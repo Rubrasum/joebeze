44bf4ccaf07819982048c08f3ec7054dfd9ed344
Rubrasum <josephbetbeze@gmail.com>
2023-05-11 00:56:10 -0500

    Replaced home view entirely.

--- resources/views/components/posts/feed.blade.php
+++ resources/views/components/posts/feed.blade.php
@@ -1,8 +1,13 @@
 @props(['posts'])
-<x-posts.featured-card :post="$posts[0]"/>
 
-@if ($posts->count() > 1)
-    @foreach ($posts->skip(1) as $post)
-        <x-posts.card :post="$post" />
-    @endforeach
-@endif
+<main>
+
+    <x-posts.featured-card :post="$posts[0]"/>
+
+    @if ($posts->count() > 1)
+        @foreach ($posts->skip(1) as $post)
+            <x-posts.card :post="$post" />
+        @endforeach
+    @endif
+
+</main>
