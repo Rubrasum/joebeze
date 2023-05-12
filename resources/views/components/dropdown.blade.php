59d5d269dc72dc582e88ed7bb58a66a2d14e844e
Rubrasum <josephbetbeze@gmail.com>
2023-05-12 18:38:14 -0500

    Added the generic dropdown component, and the specialized categories dropwdown.

---
+++ resources/views/components/dropdown.blade.php
@@ -0,0 +1,10 @@
+<div x-data="{ show: false }" @click.away="show = false">
+    {{--  trigger  --}}
+    <div @click="show = ! show">
+        {{ $trigger }}
+    </div>
+    {{--  Links--}}
+    <div x-show="show" class="inset-x-0 mt-1 py-2 absolute bg-white w-full rounded-b-xl z-50 overflow-auto max-h-52" style="display:none">
+        {{ $slot }}
+    </div>
+</div>
