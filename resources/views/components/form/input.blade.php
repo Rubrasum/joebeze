8aa10b16523821bfcd274e0e90981d349fc81fb4
Rubrasum <josephbetbeze@gmail.com>
2023-05-18 00:06:39 -0500

    Created a basic input component

---
+++ resources/views/components/form/input.blade.php
@@ -0,0 +1,13 @@
+@props(['name'])
+
+<x-form.field>
+    <x-form.label name="{{ $name }}" class="text-white"/>
+
+    <input class="border border-gray-800 bg-gray-900 text-white p-2 w-full rounded"
+           name="{{ $name }}"
+           id="{{ $name }}"
+        {{ $attributes(['value' => old($name)]) }}
+    >
+
+    <x-form.error name="{{ $name }}" class="text-white"/>
+</x-form.field>
