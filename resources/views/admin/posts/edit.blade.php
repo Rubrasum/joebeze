9c767c0332ca839ab5e569b1fb48c6c51012f523
Rubrasum <josephbetbeze@gmail.com>
2023-05-18 00:15:06 -0500

    Filled out the edit post form

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,9 +1,32 @@
 @extends('layout.full')
 
 @section('content')
-    <x-setting :heading="'Edit Post: ' . $post->title">
+    <x-dashpage :heading="'Edit Post: ' . $post->title">
         <form method="POST" action="/admin/posts/{{ $post->id }}" enctype="multipart/form-data">
             @csrf
+            @method('PATCH')
+
+            <x-form.input name="title" :value="old('title', $post->title)" required />
+            <x-form.input name="slug" :value="old('slug', $post->slug)" required />
+
+            <x-form.textarea name="excerpt" required height="48">{{ old('excerpt', $post->excerpt) }}</x-form.textarea>
+            <x-form.textarea name="body" required height="96">{{ old('body', $post->body) }}</x-form.textarea>
+
+            <x-form.field>
+                <x-form.label name="category"/>
+
+                <select name="category_id" id="category_id" required class="border border-gray-800 bg-gray-900 text-white p-2 rounded">
+                    @foreach (\App\Models\Category::all() as $category)
+                        <option class="bg-gray-900 text-white" value="{{ $category->id }}" {{ old('category_id') == $category->id ? 'selected' : '' }}>
+                            {{ ucwords($category->name) }}
+                        </option>
+                    @endforeach
+                </select>
+
+                <x-form.error name="category"/>
+            </x-form.field>
+
+            <x-form.button>Update</x-form.button>
         </form>
-    </x-setting>
+    </x-dashpage>
 @endsection
