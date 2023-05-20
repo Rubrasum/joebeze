12953d4cf15e504fccc8271af478882d6b6f5628
Rubrasum <josephbetbeze@gmail.com>
2023-05-20 02:07:34 -0500

    Added type to the input element option

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,4 +1,4 @@
-@props(['name'])
+@props(['name', 'type' => 'text'])
 
 <x-form.field>
     <x-form.label name="{{ $name }}" class="text-white"/>
@@ -6,6 +6,7 @@
     <input class="border border-gray-800 bg-gray-900 text-white p-2 w-full rounded"
            name="{{ $name }}"
            id="{{ $name }}"
+           type="{{ $type }}"
         {{ $attributes(['value' => old($name)]) }}
     >
 
