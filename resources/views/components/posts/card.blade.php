76eef8a0f67323a94f52bc15875c6804ce12b82b
Rubrasum <josephbetbeze@gmail.com>
2023-05-20 02:07:44 -0500

    Updated card styling for images

--- resources/views/components/posts/card.blade.php
+++ resources/views/components/posts/card.blade.php
@@ -3,7 +3,7 @@
 <div class="bg-black">
     <div class="w-full relative">
         <div class="top-0 left-0 w-full bg-black bg-opacity-60 flex flex-col justify-between ">
-            <div class="container mx-auto px-8 py-5 mb-12 grid
+            <div class="container mx-auto px-16 py-5 mb-12 grid items-center
                         grid-cols-10 gap-5 border-l-8 border-r-2 border-b-4 border-t-8
                         border-gray-800 rounded-br-md
                         ">
@@ -13,7 +13,7 @@
                 <div class="col-span-8 flex">
                     <div class="w-2/5 pr-3">
                         <a href="/post/{{ $post->slug }}"><h2 class="text-white text-2xl font-bold hover:underline">{{ $post->title }}</h2></a>
-                        <p class="text-white my-2">Published on: <span class="font-semibold">{{ $post->created_at->diffForHumans() }}</span></p>
+                        <p class="text-white my-2">Published: <span class="font-semibold">{{ $post->created_at->toFormattedDateString() }}</span></p>
                     </div>
                     <div class="w-3/5 text-white">
                         <p class="mb-4 text-justify">{!! $post->excerpt !!}</p>
