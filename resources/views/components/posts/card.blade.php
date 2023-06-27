ffa81e478280c4365799d91a8f171d7069fdc03e
Rubrasum <josephbetbeze@gmail.com>
2023-06-26 21:33:10 -0500

    Reverted the stack at smaller sizes thing.

--- resources/views/components/posts/card.blade.php
+++ resources/views/components/posts/card.blade.php
@@ -2,19 +2,20 @@
 
 <div class="bg-black">
     <div class="w-full relative">
-        <div class="top-0 left-0 w-full bg-black bg-opacity-60 flex flex-col justify-between">
-            <div class="container mx-auto px-4 sm:px-6 lg:px-16 py-5 mb-12 grid items-center
-                        md:grid-cols-10 grid-cols-1 gap-5 border-l-8 border-r-2 border-b-4 border-t-8
-                        border-gray-800 rounded-br-md">
-                <div class="col-span-2 md:col-span-2">
+        <div class="top-0 left-0 w-full bg-black bg-opacity-60 flex flex-col justify-between ">
+            <div class="container mx-auto px-16 py-5 mb-12 grid items-center
+                        grid-cols-10 gap-5 border-l-8 border-r-2 border-b-4 border-t-8
+                        border-gray-800 rounded-br-md
+                        ">
+                <div class="col-span-2 ">
                     <img src="{{ asset('images/' .$post->category->name. '-cat-logo.jpg') }}" alt="Image" class="w-full h-auto border-2 border-gray-800">
                 </div>
-                <div class="col-span-8 md:col-span-8 flex flex-col md:flex-row">
-                    <div class="w-full md:w-2/5 pr-3">
+                <div class="col-span-8 flex">
+                    <div class="w-2/5 pr-3">
                         <a href="/post/{{ $post->slug }}"><h2 class="text-white text-2xl font-bold">{{ $post->title }}</h2></a>
                         <p class="text-white my-2">Published: <span class="font-semibold">{{ $post->published_at->toFormattedDateString() }}</span></p>
                     </div>
-                    <div class="w-full md:w-3/5 text-white mt-4 md:mt-0">
+                    <div class="w-3/5 text-white">
                         <div class="mb-4 text-justify">{!! $post->excerpt !!}</div>
                         <div class="flex justify-between items-center">
                             <div class="space-x-2">
@@ -29,6 +30,9 @@
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class="col-span-1 text-white flex items-end justify-center h-full">
+
                 </div>
             </div>
         </div>
