7e7c8edcb9a956ac33cc2345446f853b84e2e121
Rubrasum <josephbetbeze@gmail.com>
2023-06-26 22:03:14 -0500

    Fixed cards up to md breakpoint.

--- resources/views/components/posts/card.blade.php
+++ resources/views/components/posts/card.blade.php
@@ -3,22 +3,22 @@
 <div class="bg-black">
     <div class="w-full relative">
         <div class="top-0 left-0 w-full bg-black bg-opacity-60 flex flex-col justify-between ">
-            <div class="container mx-auto xl:px-16 px-4 py-5 mb-12 grid items-center
-                        grid-cols-10 gap-5 border-l-8 border-r-2 border-b-4 border-t-8
+            <div class="container mx-auto xl:px-16 px-4 py-5 mb-12 md:grid items-center
+                        md:grid-cols-10 md:gap-5 border-l-8 border-r-2 border-b-4 border-t-8
                         border-gray-800 rounded-br-md
                         ">
-                <div class="col-span-2 h-full">
+                <div class="md:col-span-2 h-full">
                     <img src="{{ asset('images/' .$post->category->name. '-cat-logo.jpg') }}"
                          alt="Image" class="w-full h-auto border-2 border-gray-800">
                 </div>
-                <div class="col-span-8 flex">
-                    <div class="w-2/5 pr-3 items-center md:items-start md:flex-row md:items-baseline md:space-x-3">
+                <div class="md:col-span-8 md:flex">
+                    <div class="md:w-2/5 pr-3 items-center md:items-start md:flex-row md:items-baseline md:space-x-3">
                         <a href="/post/{{ $post->slug }}"><h2 class="text-white text-2xl font-bold">{{ $post->title }}</h2></a>
                         <p class="text-white my-2">Published: <span class="font-semibold"
                             >{{ $post->published_at->toFormattedDateString() }}</span></p>
                     </div>
 
-                    <div class="w-3/5 text-white">
+                    <div class="md:w-3/5 text-white">
                         <div class="mb-4 text-justify text-xs xl:text-base">{!! $post->excerpt !!}</div>
                         <div class="flex justify-between items-center">
                             <div class="space-x-2">
