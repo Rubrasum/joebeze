82ebdc9d1095038985724f1f9df611c5a90ab7ba
Rubrasum <josephbetbeze@gmail.com>
2024-03-23 11:05:17 -0400

    Fixed seeding for posts so that it doesn't run out of color options too quickly.

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -19,21 +19,24 @@ class CategoryFactory extends Factory
      */
     public function definition(): array
     {
+        // TODO Update this so that categories can share colors
         $tailwindBorderColors = array(
-            'red-200',
-            'yellow-200',
-            'green-200',
-            'blue-200',
-            'indigo-200',
-            'purple-200',
-            'pink-200',
-            'red-500',
-            'yellow-500',
-            'green-500',
-            'blue-500',
-            'indigo-500',
-            'purple-500',
-            'pink-500'
+            'red-100', 'red-200', 'red-300',
+            'orange-100', 'orange-200', 'orange-300',
+            'amber-100', 'amber-200', 'amber-300',
+            'yellow-100', 'yellow-200', 'yellow-300',
+            'lime-100', 'lime-200', 'lime-300',
+            'green-100', 'green-200', 'green-300',
+            'emerald-100', 'emerald-200', 'emerald-300',
+            'teal-100', 'teal-200', 'teal-300',
+            'cyan-100', 'cyan-200', 'cyan-300',
+            'blue-100', 'blue-200', 'blue-300',
+            'indigo-100', 'indigo-200', 'indigo-300',
+            'violet-100', 'violet-200', 'violet-300',
+            'purple-100', 'purple-200', 'purple-300',
+            'fuchsia-100', 'fuchsia-200', 'fuchsia-300',
+            'pink-100', 'pink-200', 'pink-300',
+            'rose-100', 'rose-200', 'rose-300'
         );
 
         return [
