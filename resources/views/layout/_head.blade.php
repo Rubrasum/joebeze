55dee3f2324859e1ad46d0bd492c10e164086fa6
Rubrasum <josephbetbeze@gmail.com>
2024-03-24 15:50:45 -0400

    Aded the new logo and the laser effect (works but unfinished styling).

--- resources/views/layout/_head.blade.php
+++ resources/views/layout/_head.blade.php
@@ -7,8 +7,6 @@
 <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 <link rel="preconnect" href="https://fonts.gstatic.com" />
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet" />
-<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css" />
-<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css" />
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
 
@@ -17,7 +15,8 @@
 <script src="https://cdn.tailwindcss.com"></script>
 <script src="//code.jquery.com/jquery-1.10.2.js"></script>
 <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
-
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
 
 
 
--- resources/views/layout/_nav.blade.php
+++ resources/views/layout/_nav.blade.php
@@ -7,13 +7,42 @@
         <nav class="flex justify-between items-center py-4">
             <div class="w-4/5 sm:w-3/5 md:w-2/5 lg:w-2/5">
                 <a href="/">
-                    <img src="/images/bytebeze-logo.png" alt="Logo">
+                    <div id="logo-container">
+                        <svg id="joebeze_logo" data-name="joebeze_logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 518.16 54.21">
+                            <defs>
+                                <style>
+                                    .cls-1 {
+                                        fill: #fff;
+                                        stroke-width: 0px;
+                                    }
+                                </style>
+                            </defs>
+                            <path class="cls-1" d="M26.82,4.59v24.75c0,7.56-3.98,13.05-14,13.05H0v-6.61h12.43c5.94,0,7.73-2.86,7.73-6.5V4.59h6.66Z"/>
+                            <path class="cls-1" d="M52.92,12.82c7.84,0,12.82,4.82,12.82,14.78s-4.98,14.78-12.82,14.78h-6.22c-7.9,0-12.82-4.82-12.82-14.78s4.93-14.78,12.82-14.78h6.22ZM47.26,18.54c-5.15,0-7.39,2.69-7.39,9.07s2.24,9.02,7.39,9.02h5.1c5.1,0,7.39-2.63,7.39-9.02s-2.3-9.07-7.39-9.07h-5.1Z"/>
+                            <path class="cls-1" d="M92.12,12.82c7,0,9.97,3.36,9.97,9.07s-2.74,8.51-10.02,8.51h-14.06v2.13c0,2.35,1.29,4.09,4.54,4.09h18.09v5.77h-18.09c-7.39,0-10.3-4.14-10.3-9.74v-10.08c0-5.6,2.91-9.74,10.3-9.74h9.58ZM78.01,25.76h13.89c3.19,0,4.37-1.34,4.37-3.53s-1.29-3.7-4.2-3.7h-9.52c-3.25,0-4.54,1.79-4.54,4.09v3.14Z"/>
+                            <path class="cls-1" d="M115.3,0v12.82h13.1c7.84,0,12.77,4.82,12.77,14.78s-4.93,14.78-12.77,14.78h-8.18c-7.11,0-10.86-4.14-10.86-12.66V0h5.94ZM115.3,18.54v10.92c0,5.04,1.85,7.17,6.66,7.17h5.82c5.15,0,7.39-2.63,7.39-9.02s-2.24-9.07-7.39-9.07h-12.49Z"/>
+                            <path class="cls-1" d="M167.55,12.82c7,0,9.97,3.36,9.97,9.07s-2.74,8.51-10.02,8.51h-14.06v2.13c0,2.35,1.29,4.09,4.54,4.09h18.09v5.77h-18.09c-7.39,0-10.3-4.14-10.3-9.74v-10.08c0-5.6,2.91-9.74,10.3-9.74h9.58ZM153.44,25.76h13.89c3.19,0,4.37-1.34,4.37-3.53s-1.29-3.7-4.2-3.7h-9.52c-3.25,0-4.54,1.79-4.54,4.09v3.14Z"/>
+                            <path class="cls-1" d="M204.01,12.82c5.15,0,7.62,2.69,7.62,6.78,0,3.64-1.96,5.99-5.04,7.11l-16.97,6.16c-1.01.39-1.57,1.06-1.57,2.02,0,1.12.73,1.79,2.24,1.79h20.72v5.71h-21.06c-5.1,0-7.62-2.69-7.62-6.78,0-3.3,1.62-5.94,5.04-7.17l17.02-6.1c1.23-.45,1.57-1.29,1.57-2.07,0-1.12-.73-1.74-2.3-1.74h-20.27v-5.71h20.61Z"/>
+                            <path class="cls-1" d="M237.27,12.82c7,0,9.97,3.36,9.97,9.07s-2.74,8.51-10.02,8.51h-14.06v2.13c0,2.35,1.29,4.09,4.54,4.09h18.09v5.77h-18.09c-7.39,0-10.3-4.14-10.3-9.74v-10.08c0-5.6,2.91-9.74,10.3-9.74h9.58ZM223.16,25.76h13.89c3.19,0,4.37-1.34,4.37-3.53s-1.29-3.7-4.2-3.7h-9.52c-3.25,0-4.54,1.79-4.54,4.09v3.14Z"/>
+                            <path class="cls-1" d="M299.93,0v29.74c0,8.51-3.81,12.66-10.86,12.66h-8.18c-7.9,0-12.82-4.82-12.82-14.78s4.93-14.78,12.82-14.78h13.05V0h5.99ZM293.94,29.46v-10.92h-12.49c-5.1,0-7.39,2.69-7.39,9.07s2.3,9.02,7.39,9.02h5.88c4.76,0,6.61-2.13,6.61-7.17Z"/>
+                            <path class="cls-1" d="M327.65,12.82c7,0,9.97,3.36,9.97,9.07s-2.74,8.51-10.02,8.51h-14.06v2.13c0,2.35,1.29,4.09,4.54,4.09h18.09v5.77h-18.09c-7.39,0-10.3-4.14-10.3-9.74v-10.08c0-5.6,2.91-9.74,10.3-9.74h9.58ZM313.54,25.76h13.89c3.19,0,4.37-1.34,4.37-3.53s-1.29-3.7-4.2-3.7h-9.52c-3.25,0-4.54,1.79-4.54,4.09v3.14Z"/>
+                            <path class="cls-1" d="M346.97,12.82l9.86,23.74c.22.62.73.9,1.46.9.78,0,1.29-.28,1.57-.9l10.25-23.74h6.38l-11.42,26.38c-1.23,2.8-3.42,3.86-6.78,3.86s-5.6-1.12-6.72-3.86l-10.86-26.38h6.27Z"/>
+                            <path class="cls-1" d="M402.69,0v12.82h13.1c7.84,0,12.77,4.82,12.77,14.78s-4.93,14.78-12.77,14.78h-8.18c-7.11,0-10.86-4.14-10.86-12.66V0h5.94ZM402.69,18.54v10.92c0,5.04,1.85,7.17,6.66,7.17h5.82c5.15,0,7.39-2.63,7.39-9.02s-2.24-9.07-7.39-9.07h-12.49Z"/>
+                            <path class="cls-1" d="M441.61,0v42.39h-5.94V0h5.94Z"/>
+                            <path class="cls-1" d="M467.76,12.82c7.84,0,12.82,4.82,12.82,14.78s-4.98,14.78-12.82,14.78h-6.22c-7.9,0-12.82-4.82-12.82-14.78s4.93-14.78,12.82-14.78h6.22ZM462.11,18.54c-5.15,0-7.39,2.69-7.39,9.07s2.24,9.02,7.39,9.02h5.1c5.1,0,7.39-2.63,7.39-9.02s-2.3-9.07-7.39-9.07h-5.1Z"/>
+                            <path class="cls-1" d="M507.3,12.82c7.06,0,10.86,4.14,10.86,12.66v17.02c0,8.4-2.91,11.7-11.2,11.7h-17.58v-5.71h16.91c4.42,0,5.88-1.74,5.88-5.99v-1.34h-13.05c-7.9,0-12.82-4.65-12.82-14.22s4.93-14.11,12.82-14.11h8.18ZM499.68,35.78h12.49v-10.36c0-4.82-1.85-6.89-6.61-6.89h-5.88c-5.1,0-7.39,2.58-7.39,8.62s2.3,8.62,7.39,8.62Z"/>
+                        </svg>
+                        <canvas id="canvas" width="518" height="54"></canvas>
+                        <canvas id="canvas2" width="518" height="54"></canvas>
+                    </div>
+
                 </a>
             </div>
             <div class="hidden lg:flex xl:w-2/5 lg:w-0/5 items-center justify-center">
             </div>
             <div class="hidden lg:flex lg:w-3/5 justify-end">
-                <div class="w-2/5 relative flex lg:inline-flex mr-10 lg:mr-5 bg-white text-gray-800 border-2 border-gray-800 hover:border-gray-500 rounded-md">
+                <div
+                    class="w-2/5 relative flex lg:inline-flex mr-10 lg:mr-5 bg-white text-gray-800 border-2 border-gray-800 hover:border-gray-500 rounded-md">
                     <x-categories.dropdown :post="$post ?? null"/>
                 </div>
                 <div x-data="{ search: '{{ request('search') }}', category: '{{ request('category') }}' }">
@@ -33,10 +62,13 @@
                 </div>
             </div>
             <div class="lg:hidden w-1/5 flex items-center justify-center">
-                <button @click="open = !open" class="border border-white rounded-md p-2 bg-gray-800 inline-block cursor-pointer">
+                <button @click="open = !open"
+                        class="border border-white rounded-md p-2 bg-gray-800 inline-block cursor-pointer">
                     <svg class="fill-current text-white h-6 w-6" viewBox="0 0 24 24">
-                        <path x-show="!open" fill-rule="evenodd" clip-rule="evenodd" d="M4 5h16a1 1 0 010 2H4a1 1 0 110-2zm0 6h16a1 1 0 010 2H4a1 1 0 010-2zm0 6h16a1 1 0 010 2H4a1 1 0 010-2z"/>
-                        <path x-show="open" fill-rule="evenodd" clip-rule="evenodd" d="M12.707 17.707a1 1 0 01-1.414 0l-5-5a1 1 0 011.414-1.414L12 14.586l4.293-4.293a1 1 0 111.414 1.414l-5 5z"/>
+                        <path x-show="!open" fill-rule="evenodd" clip-rule="evenodd"
+                              d="M4 5h16a1 1 0 010 2H4a1 1 0 110-2zm0 6h16a1 1 0 010 2H4a1 1 0 010-2zm0 6h16a1 1 0 010 2H4a1 1 0 010-2z"/>
+                        <path x-show="open" fill-rule="evenodd" clip-rule="evenodd"
+                              d="M12.707 17.707a1 1 0 01-1.414 0l-5-5a1 1 0 011.414-1.414L12 14.586l4.293-4.293a1 1 0 111.414 1.414l-5 5z"/>
                     </svg>
                 </button>
             </div>
@@ -44,10 +76,12 @@
 
         <div x-show="open" class="lg:hidden">
             <div class="lg:border-t border-gray-700 pt-4 flex mb-2">
-                <div class="w-2/5 relative flex lg:inline-flex mr-10 lg:mr-5 bg-white text-gray-800 border-2 border-gray-800 hover:border-gray-500 rounded-md">
+                <div
+                    class="w-2/5 relative flex lg:inline-flex mr-10 lg:mr-5 bg-white text-gray-800 border-2 border-gray-800 hover:border-gray-500 rounded-md">
                     <x-categories.dropdown :post="$post ?? null"/>
                 </div>
-                <div class="w-2/5 relative flex lg:inline-flex mr-10 lg:mr-5 bg-white text-gray-800 border-2 border-gray-800 hover:border-gray-500 rounded-md">
+                <div
+                    class="w-2/5 relative flex lg:inline-flex mr-10 lg:mr-5 bg-white text-gray-800 border-2 border-gray-800 hover:border-gray-500 rounded-md">
                     <form class="w-full" method="GET" action="#">
                         @if (request('category'))
                             <input type="hidden" name="category" value="{{ request('category') }}">
@@ -63,4 +97,226 @@
             </div>
         </div>
     </div>
+    <script>
+        // delay til page load
+        document.addEventListener("DOMContentLoaded", () => {
+            class Particle {
+                constructor(x, y, size) {
+                    this.speed = 0.6;
+                    this.x = x;
+                    this.y = y;
+                    this.vx = (Math.random() - 0.5) * this.speed;
+                    this.vy = (Math.random() - 0.5) * this.speed;
+                    this.opacity = 1;
+                    this.size = size;
+                }
+
+                draw(context) {
+                    this.opacity -= this.speed / 200;
+                    context.fillStyle = `rgba(0, 255, 0, ${this.opacity})`;
+                    context.fillRect(this.x, this.y, this.size, this.size);
+                }
+
+                move() {
+                    this.x += this.vx;
+                    this.y += this.vy + (1 - this.opacity) * this.speed;
+                }
+            }
+
+            class Laser {
+                constructor(x, y) {
+                    this.x = x;
+                    this.y = y;
+                    this.steps = [];
+                    this.particles = [];
+                }
+
+                drawTo(x, y, ctx) {
+                    ctx.beginPath();
+                    ctx.moveTo(this.x, this.y);
+                    ctx.lineTo(x, y);
+                    ctx.stroke();
+                    ctx.fillStyle = "red";
+                    ctx.fillRect(x - 2.5, y - 2.5, 5, 5); // Adjust the position of the dot
+                }
+
+                drawEnd(ctx) {
+                    var p = this.steps.shift();
+                    if (p !== undefined) {
+                        this.drawTo(p[0], p[1], ctx);
+                    } else {
+                        var index = this.particles.length;
+                        while (index--) {
+                            var p = this.particles[index];
+                            p.move();
+                            if (p.opacity < 0 || p.x < 0 || p.x > ctx.canvas.width || p.y < 0 || p.y > ctx.canvas.height) {
+                                this.particles.splice(index, 1);
+                            } else {
+                                p.draw(ctx);
+                            }
+                        }
+                    }
+                }
+
+                endFrom(x0, y0) {
+                    var nrOfSteps = 800;
+                    var i = nrOfSteps;
+                    while (i--) {
+                        var x = this.x + i * (x0 - this.x) / nrOfSteps + Math.cos(i / 10) * i / 4;
+                        var y = this.y + i * (y0 - this.y) / nrOfSteps + Math.sin(i / 10) * i / 4;
+                        this.steps.push([x, y]);
+                        if (i < 100) {
+                            this.particles.push(new Particle(x, y, 2));
+                        }
+                    }
+                }
+            }
+
+            class LaserWriter {
+                constructor(canvasId, canvas2Id, svgId) {
+                    var canvas = document.getElementById(canvasId);
+                    var canvas2 = document.getElementById(canvas2Id);
+                    this.svg = document.getElementById(svgId);
+                    this.ctx = canvas.getContext("2d");
+                    this.ctx2 = canvas2.getContext("2d");
+                    this.tick = 0;
+                    this.pointsIndex = 0;
+                    this.points = [];
+                    this.size = 3;
+                    this.initCanvasSize();
+                }
+
+                initCanvasSize() {
+                    const viewBox = this.svg.viewBox.baseVal;
+                    const scaleFactor = window.devicePixelRatio || 1; // Adjust for high-density displays
+
+                    this.w = viewBox.width * scaleFactor;
+                    this.h = viewBox.height * scaleFactor;
+
+                    this.ctx.canvas.width = this.w;
+                    this.ctx.canvas.height = this.h;
+                    this.ctx2.canvas.width = this.w;
+                    this.ctx2.canvas.height = this.h;
+
+                    // Get the SVG logo container element
+                    const logoContainer = this.svg.parentNode;
+
+                    // Set the canvases' positions and dimensions to match the SVG logo
+                    this.ctx.canvas.style.position = 'absolute';
+                    this.ctx.canvas.style.left = `${logoContainer.offsetLeft}px`;
+                    this.ctx.canvas.style.top = `${logoContainer.offsetTop}px`;
+                    this.ctx.canvas.style.width = `${logoContainer.offsetWidth}px`;
+                    this.ctx.canvas.style.height = `${logoContainer.offsetHeight}px`;
+
+                    this.ctx2.canvas.style.position = 'absolute';
+                    this.ctx2.canvas.style.left = `${logoContainer.offsetLeft}px`;
+                    this.ctx2.canvas.style.top = `${logoContainer.offsetTop}px`;
+                    this.ctx2.canvas.style.width = `${logoContainer.offsetWidth}px`;
+                    this.ctx2.canvas.style.height = `${logoContainer.offsetHeight}px`;
+                }
+
+                init(text, size) {
+                    // Set font size for the text
+                    this.ctx.font = `${size}px nasalisation`;
+                    // Calculate starting X position to center the text
+                    this.startX = (this.w - this.ctx.measureText(text).width) / 2;
+                    // Draw text onto the canvas
+                    this.ctx.fillText(text, this.startX, 100);
+
+                    // Get the imageData of the text
+                    var image = this.ctx.getImageData(0, 0, this.w, this.h);
+                    var buffer32 = new Uint32Array(image.data.buffer);
+
+                    // Clear the canvas after getting the image data
+                    this.ctx.clearRect(0, 0, this.w, this.h);
+
+                    // Set the laser beam styles
+                    this.ctx2.strokeStyle = "rgba(20, 255, 70, 1)";
+                    this.ctx2.lineCap = "round";
+                    this.ctx2.lineWidth = 4;
+
+                    // Find the top-left non-zero pixel to set the laser start position
+                    let startX = this.w, startY = this.h; // Initialize with max values
+                    for (let y = 0; y < this.h; y++) {
+                        for (let x = 0; x < this.w; x++) {
+                            if (buffer32[y * this.w + x]) {
+                                startX = Math.min(startX, x);
+                                startY = Math.min(startY, y);
+                            }
+                        }
+                    }
+
+                    // Initialize the laser at the top-left of the text
+                    this.laserStart.x = startX;
+                    this.laserStart.y = startY;
+                    this.laser = new Laser(this.laserStart.x, this.laserStart.y);
+
+                    // Iterate over every pixel to find where the text is drawn
+                    for (var y = 0; y < this.h; y++) {
+                        for (var x = 0; x < this.w; x++) {
+                            if (buffer32[y * this.w + x]) {
+                                this.points.push([x, y]);
+                            }
+                        }
+                    }
+                }
+
+                draw() {
+                    this.ctx2.clearRect(0, 0, this.w, this.h);
+
+                    if (this.pointsIndex < this.points.length) {
+                        var p = this.points[this.pointsIndex];
+                        this.x = p[0];
+                        this.y = p[1];
+                        this.laser.drawTo(this.x, this.y, this.ctx2);
+
+                        if (this.tick % 2 === 0) {
+                            var particle = new Particle(this.x, this.y, this.size);
+                            this.laser.particles.push(particle);
+                        }
+                    } else if (this.pointsIndex === this.points.length) {
+                        this.laser.endFrom(this.x, this.y);
+                    } else {
+                        this.laser.drawEnd(this.ctx2);
+                    }
+
+                    this.pointsIndex++;
+                    this.tick++;
+
+                    if (this.pointsIndex <= this.points.length + 400) {
+                        requestAnimationFrame(() => this.draw());
+                    }
+                }
+
+                traceSVG() {
+                    const paths = this.svg.querySelectorAll('path');
+                    paths.forEach(path => {
+                        const length = path.getTotalLength();
+                        for (let i = 0; i < length; i++) {
+                            const point = path.getPointAtLength(i);
+                            this.points.push([point.x, point.y]);
+                        }
+                    });
+
+                    this.laser = new Laser(this.points[0][0], this.points[0][1]);
+                }
+            }
+
+
+            const laserWriter = new LaserWriter("canvas", "canvas2", "joebeze_logo");
+            laserWriter.traceSVG();
+            laserWriter.draw();
+        });
+    </script>
+
+    <style>
+
+        #logo-container canvas {
+            position: absolute;
+            top: 0;
+            left: 0;
+        }
+    </style>
+
 </div>
+
