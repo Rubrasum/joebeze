018cf7f188e0e1c387bd0f073b90e450be377667
Rubrasum <josephbetbeze@gmail.com>
2024-04-08 09:46:13 -0400

    Vertically aligned the categories drop down and search.

--- resources/views/layout/_nav.blade.php
+++ resources/views/layout/_nav.blade.php
@@ -1,5 +1,5 @@
 <div id="nav_container" class="h-30" x-data="{ open: false }"
-     style="background-image: url('{{ asset('images/bytebeze-banner-tess.png') }}'); background-repeat: repeat;min-height: 96px;">
+     style="background-image: url('{{ asset('images/bytebeze-banner-tess.png') }}'); background-repeat: repeat;min-height: 96px; height: 96px;">
     <a href="/">
         <div id="logo-container" class="absolute m-2 top-0 left-0 ml-[10vw] " style="min-height: 76px;width:650px; border-radius: 0.5rem;">
 
@@ -114,93 +114,91 @@
                 </svg>
                 </div>
                 <div style="position: relative;">
-                <svg id="ripple-effect-bg" style="
-                    z-index: 19; position: absolute; top: -352px; left: -80px; width: 950px; height: 800px;
-                    border-radius: 0.5rem; box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1);
-                    border: solid white 2px; transition: all 1000ms ease-in-out;" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg"
-                     xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1070.78 901.04">
-                    <defs>
-                        <style>
-                            .cls-3-1 {
-                                clip-path: url(#clippath-2);
-                            }
+                    <svg id="ripple-effect-bg" style=" z-index: 19; position: absolute; top: -352px; left: -80px; width: 950px; height: 800px;
+                        border-radius: 0.5rem; box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1);
+                        border: solid white 2px; transition: all 1000ms ease-in-out;" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg"
+                         xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1070.78 901.04">
+                        <defs>
+                            <style>
+                                .cls-3-1 {
+                                    clip-path: url(#clippath-2);
+                                }
 
-                            .cls-3-2 {
-                                fill: none;
-                                stroke-width: 0px;
-                            }
+                                .cls-3-2 {
+                                    fill: none;
+                                    stroke-width: 0px;
+                                }
 
-                            .cls-3-3 {
-                                clip-path: url(#clippath-1);
-                            }
+                                .cls-3-3 {
+                                    clip-path: url(#clippath-1);
+                                }
 
-                            .cls-3-4 {
-                                clip-path: url(#clippath-4);
-                            }
+                                .cls-3-4 {
+                                    clip-path: url(#clippath-4);
+                                }
 
-                            .cls-3-5 {
-                                clip-path: url(#clippath);
-                            }
+                                .cls-3-5 {
+                                    clip-path: url(#clippath);
+                                }
 
-                            .cls-3-6 {
-                                clip-path: url(#clippath-3);
-                            }
+                                .cls-3-6 {
+                                    clip-path: url(#clippath-3);
+                                }
 
-                            .cls-3-7 {
-                                clip-path: url(#clippath-5);
-                            }
-                        </style>
-                        <clipPath id="clippath">
-                            <circle id="_EllipseWhite10_" data-name="&amp;lt;EllipseWhite10&amp;gt;" class="cls-3-2" cx="-100" cy="-100" r="1"/>
-                        </clipPath>
-                        <clipPath id="clippath-1">
-                            <circle class="cls-3-2" cx="-100" cy="-100" r="1"/>
-                        </clipPath>
-                        {{--                                <clipPath id="clippath-2">--}}
-                        {{--                                    <circle class="cls-3-2" cx="-100" cy="-100" r="1"/>--}}
-                        {{--                                </clipPath>--}}
-                        <clipPath id="clippath-3">
-                            <circle class="cls-3-2" cx="-100" cy="-100" r="1"/>
-                        </clipPath>
-                        <clipPath id="clippath-4">
-                            <circle class="cls-3-2" cx="-100" cy="-100" r="1"/>
-                        </clipPath>
-                        <clipPath id="clippath-5">
-                            <circle class="cls-3-2" cx="-100" cy="-100" r="1"/>
-                        </clipPath>
-                    </defs>
-                    <g id="_Clip_Group_Redhead_" data-name="&amp;lt;Clip Group Redhead&amp;gt;">
-                        <g class="cls-3-5">
-                            <image width="1017" height="284" transform="translate(80.85 331.18) scale(.76)" xlink:href="{{ asset('images/redhead.jpg') }}"/>
+                                .cls-3-7 {
+                                    clip-path: url(#clippath-5);
+                                }
+                            </style>
+                            <clipPath id="clippath">
+                                <circle id="_EllipseWhite10_" data-name="&amp;lt;EllipseWhite10&amp;gt;" class="cls-3-2" cx="-100" cy="-100" r="1"/>
+                            </clipPath>
+                            <clipPath id="clippath-1">
+                                <circle class="cls-3-2" cx="-100" cy="-100" r="1"/>
+                            </clipPath>
+                            {{--                                <clipPath id="clippath-2">--}}
+                            {{--                                    <circle class="cls-3-2" cx="-100" cy="-100" r="1"/>--}}
+                            {{--                                </clipPath>--}}
+                            <clipPath id="clippath-3">
+                                <circle class="cls-3-2" cx="-100" cy="-100" r="1"/>
+                            </clipPath>
+                            <clipPath id="clippath-4">
+                                <circle class="cls-3-2" cx="-100" cy="-100" r="1"/>
+                            </clipPath>
+                            <clipPath id="clippath-5">
+                                <circle class="cls-3-2" cx="-100" cy="-100" r="1"/>
+                            </clipPath>
+                        </defs>
+                        <g id="_Clip_Group_Redhead_" data-name="&amp;lt;Clip Group Redhead&amp;gt;">
+                            <g class="cls-3-5">
+                                <image width="1017" height="284" transform="translate(80.85 331.18) scale(.76)" xlink:href="{{ asset('images/redhead.jpg') }}"/>
+                            </g>
                         </g>
-                    </g>
-                    <g id="_Clip_Group_Blueninja_" data-name="&amp;lt;Clip Group Blueninja&amp;gt;">
-                        <g class="cls-3-3">
-                            <image width="1575" height="315" transform="translate(1220.78 205.37) rotate(-180) scale(.9 -.9)" xlink:href="{{ asset('images/blue-ninja.jpg') }}"/>
+                        <g id="_Clip_Group_Blueninja_" data-name="&amp;lt;Clip Group Blueninja&amp;gt;">
+                            <g class="cls-3-3">
+                                <image width="1575" height="315" transform="translate(1220.78 205.37) rotate(-180) scale(.9 -.9)" xlink:href="{{ asset('images/blue-ninja.jpg') }}"/>
+                            </g>
                         </g>
-                    </g>
-                    {{--                            <g id="_Clip_Group_Darkgame_" data-name="&amp;lt;Clip Group Darkgame&amp;gt;">--}}
-                    {{--                                <g class="cls-3-1">--}}
-                    {{--                                    <image width="640" height="360" transform="translate(150.31 277.3) scale(1.05)" xlink:href="{{ asset('images/dark-game.gif') }}"/>--}}
-                    {{--                                </g>--}}
-                    {{--                            </g>--}}
-                    <g id="_Clip_Group_Blackcat_" data-name="&amp;lt;Clip Group Blackcat&amp;gt;">
-                        <g class="cls-3-6">
-                            <image width="740" height="119" transform="translate(50.31 390.18) scale(1.08)" xlink:href="{{ asset('images/black-cat.jpg') }}"/>
+                        {{--                            <g id="_Clip_Group_Darkgame_" data-name="&amp;lt;Clip Group Darkgame&amp;gt;">--}}
+                        {{--                                <g class="cls-3-1">--}}
+                        {{--                                    <image width="640" height="360" transform="translate(150.31 277.3) scale(1.05)" xlink:href="{{ asset('images/dark-game.gif') }}"/>--}}
+                        {{--                                </g>--}}
+                        {{--                            </g>--}}
+                        <g id="_Clip_Group_Blackcat_" data-name="&amp;lt;Clip Group Blackcat&amp;gt;">
+                            <g class="cls-3-6">
+                                <image width="740" height="119" transform="translate(50.31 390.18) scale(1.08)" xlink:href="{{ asset('images/black-cat.jpg') }}"/>
+                            </g>
                         </g>
-                    </g>
-                    <g id="_Clip_Group_Tanwoman_" data-name="&amp;lt;Clip Group Tanwoman&amp;gt;">
-                        <g class="cls-3-4">
-                            <image width="829" height="141" transform="translate(90.61 355.43) scale(.95)" xlink:href="{{ asset('images/tan-woman.jpg') }}"/>
+                        <g id="_Clip_Group_Tanwoman_" data-name="&amp;lt;Clip Group Tanwoman&amp;gt;">
+                            <g class="cls-3-4">
+                                <image width="829" height="141" transform="translate(90.61 355.43) scale(.95)" xlink:href="{{ asset('images/tan-woman.jpg') }}"/>
+                            </g>
                         </g>
-                    </g>
-                    <g id="_Clip_Group_Orangewoman_" data-name="&amp;lt;Clip Group Orangewoman&amp;gt;">
-                        <g class="cls-3-7">
-                            <image width="1740" height="270" transform="translate(82.83 372.35) scale(.45)" xlink:href="{{ asset('images/orange-woman.jpg') }}"/>
+                        <g id="_Clip_Group_Orangewoman_" data-name="&amp;lt;Clip Group Orangewoman&amp;gt;">
+                            <g class="cls-3-7">
+                                <image width="1740" height="270" transform="translate(82.83 372.35) scale(.45)" xlink:href="{{ asset('images/orange-woman.jpg') }}"/>
+                            </g>
                         </g>
-                    </g>
-                </svg></div>
-
+                    </svg></div>
 
                 <canvas id="canvas" class="absolute top-0 left-0 w-full h-full" width="650" height="76"></canvas>
                 <canvas id="canvas2" style="z-index:99" class="absolute top-0 left-0 w-full h-full" width="650" height="76"></canvas>
@@ -208,9 +206,9 @@
         </div>
     </a>
     <div id="p5-canvas"></div>
-    <div class="container mx-auto px-4">
+    <div class="container mx-auto px-4 h-full">
 
-        <nav class="flex justify-between items-center py-4">
+        <nav class="h-full flex justify-between items-center py-4">
 
             <div class="w-full">
 
