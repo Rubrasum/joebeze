b6fdb28e187ec6cce4cc5da44ff6382a4f1c7437
Rubrasum <josephbetbeze@gmail.com>
2024-06-02 15:37:42 -0400

    Removed quill file, implemented ssr.js, updated vite.config.js, changed packages and added "type": "module"

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,26 @@
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ['resources/js/app.js', 'resources/css/app.css'], // Ensure CSS is included
+            ssr: 'resources/js/ssr.js', // Specify SSR entry point
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
-    server: {
-        hmr: {
-            host: 'localhost',
+    resolve: {
+        alias: {
+            '@': '/resources/js', // Use alias for cleaner imports
         },
     },
 });
