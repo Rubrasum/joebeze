692ad6dd805843a2b47491b2034d9e93f6c8d73d
Rubrasum <josephbetbeze@gmail.com>
2024-06-07 13:20:15 -0400

    Added all the Footer content

---
+++ resources/js/Components/Layout/Footer/Social.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="md:w-1/5 mt-8 md:mt-0 ml-8 md:ml-8">
+        <h4 class="text-2xl font-bold mb-4 text-white">Social</h4>
+        <ul class="list-none flex">
+            <li class="mr-4"><a href="https://twitter.com/joebetbeze" class="hover:text-white text-4xl"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://www.instagram.com/josephbetbeze/" class="hover:text-white text-4xl"><i class="fab fa-instagram"></i></a></li>
+        </ul>
+    </div>
+
+</template>
+
+<script setup>
+
+</script>
+
