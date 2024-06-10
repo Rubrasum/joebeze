766bc6eafe9857871d587e32c9f3a30229b2be2a
Rubrasum <josephbetbeze@gmail.com>
2024-06-10 13:11:53 -0400

    oops leftover error

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -64,8 +64,7 @@ class PostController extends Controller
             'canLogin' => Route::has('login'),
             'canRegister' => Route::has('register'),
             'laravelVersion' => Application::VERSION,
-            'phpVersion' => PHP_VERSION,
-            'categories' => $categories,
+            'phpVersion' => PHP_VERSION
         ]);
     }
 }
