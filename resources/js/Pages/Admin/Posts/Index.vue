d5e658d7e9e2400fb2b5a67523595bb136dcf146
Rubrasum <josephbetbeze@gmail.com>
2024-06-11 16:44:53 -0400

    updated the Link href with template literal

--- resources/js/Pages/Admin/Posts/Index.vue
+++ resources/js/Pages/Admin/Posts/Index.vue
@@ -7,9 +7,8 @@
     <div class="flex">
         <aside class="w-48 flex-shrink-0">
             <h4 class="font-semibold mb-4 text-white">Options</h4>
-
             <ul>
-                <Link :href="'admin.posts'"
+                <Link :href="'/admin/posts'"
                    class="text-white border-white bg-gray-700 hover:text-white hover:border-white inline-block mb-2 mr-4 rounded-lg border-2"
                 >
                     <li class="px-4 py-2">All Posts</li>
@@ -17,7 +16,7 @@
             </ul>
         </aside>
 
-        <main class="flex-1">
+        <main class="flex-1">/
             <div class="flex flex-col">
                 <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
@@ -28,7 +27,7 @@
                                     <td class="px-6 py-4 whitespace-nowrap">
                                         <div class="flex items-center">
                                             <div class="text-sm font-medium text-gray-900">
-                                                <Link href="/post/{{ post.slug }}">
+                                                <Link :href="`/post/${post.slug}`">
                                                     {{ post.title }}
                                                 </Link>
                                             </div>
@@ -36,11 +35,11 @@
                                     </td>
 
                                     <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                        <Link href="/admin/posts/{{ post.id }}/edit" class="text-blue-500 hover:text-blue-600">Edit</Link>
+                                        <Link :href="`/admin/posts/${post.id}/edit`" class="text-blue-500 hover:text-blue-600">Edit</Link>
                                     </td>
 
                                     <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                        <form method="DELETE" action="/admin/posts/{{ post.id }}">
+                                        <form method="DELETE" :action="`/admin/posts/${post.id}`">
                                             @csrf
 
                                             <button class="text-xs text-gray-400">Delete</button>
@@ -55,7 +54,6 @@
             </div>
         </main>
     </div>
-
 </template>
 
 <script setup>
