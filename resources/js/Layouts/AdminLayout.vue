3f5cdb54615b6993acd6e0b117831b37c0ebdb16
Rubrasum <josephbetbeze@gmail.com>
2024-06-11 20:56:50 -0400

    More adjustments to the admin layout

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -1,9 +1,41 @@
+<template>
+    <div class="flex flex-col min-h-screen bg-gray-800">
+
+        <AdminNav v-once/>
+        <MainContain class="my-6">
+            <div class="p-8 min-w-4xl max-w-screen-lg w-4/5 mx-auto bg-gray-900 text-white flex-grow overflow-hidden sm:rounded-lg sm:shadow">
+                <div class="-ml-4 -mt-4">
+                    <h1 class="text-2xl font-bold mb-8 pb-2 border-b border-gray-200 ">
+                        {{ title }}
+                    </h1>
+                    <div class="flex">
+                        <aside class="w-48 flex-shrink-0">
+                            <h4 class="font-semibold mb-4 text-white">Options</h4>
+                            <ul>
+                                <Link :href="'/admin/posts'"
+                                      class="text-white border-white bg-gray-700 hover:text-white hover:border-white inline-block mb-2 mr-4 rounded-lg border-2"
+                                >
+                                    <li class="px-4 py-2">All Posts</li>
+                                </Link>
+                            </ul>
+                        </aside>
+                        <slot></slot>
+                    </div>
+                </div>
+            </div>
+        </MainContain>
+
+        <Footer />
+    </div>
+</template>
+
 <script setup>
 // Importing necessary functions and components from Vue and Inertia.js
 import { Link, router } from '@inertiajs/vue3';
 import MainContain from '@/Components/Layout/MainContain.vue';
 import Footer from '@/Components/Layout/Footer.vue';
 import AdminNav from '@/Components/Layout/AdminNav.vue';
+import {ref, useSlots} from "vue";
 
 
 window.onload = function() {
@@ -11,21 +43,12 @@ window.onload = function() {
     document.body.style.visibility = 'visible';
 }
 
+const slot = useSlots();
+const title = ref(slot.title ? slot.title()[0].children : 'Admin Dashboard');
+
+
 </script>
 
-<template>
-    <div class="flex flex-col min-h-screen">
-
-        <AdminNav v-once/>
-        <MainContain class="bg-gray-800">
-            <div :class="`p-8 min-w-4xl max-w-screen-lg w-4/5 mx-auto bg-gray-900 text-white flex-grow`">
-                <slot></slot>
-            </div>
-        </MainContain>
-
-        <Footer />
-    </div>
-</template>
 
 <style>
 
