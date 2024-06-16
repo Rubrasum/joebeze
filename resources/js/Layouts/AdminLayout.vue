3be04919710acfa921b7b797c74388163d82a934
Rubrasum <josephbetbeze@gmail.com>
2024-06-16 12:00:54 -0400

    Fixed body not showing sometime on admin

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -58,7 +58,5 @@ const title = ref(slot.title ? slot.title()[0].children : 'Admin Dashboard');
 html {
     scroll-behavior: smooth
 }
-body {
-    visibility: hidden;
-}
+// TODO: Reapply the flicker avoiding this body -> visibility hidden
 </style>
