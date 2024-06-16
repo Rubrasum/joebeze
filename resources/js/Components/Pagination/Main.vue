aaeecf9445465b31c39110b8ae44d8f9519dc7a0
Rubrasum <josephbetbeze@gmail.com>
2024-06-16 12:00:18 -0400

    Customized the pagination further with the Next/Previous hero icons

--- resources/js/Components/Pagination/Main.vue
+++ resources/js/Components/Pagination/Main.vue
@@ -28,18 +28,30 @@
                 <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
                     <!-- Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" -->
                      <Link v-for="link in items.links"
-                           :disabled="link.active"
-                           href="link.url" aria-current="page"
-                           class="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold
-                           text-white focus:z-20 focus-visible:outline focus-visible:outline-2
-                           focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                     >{{ link.label }}</Link>
+                           :href="link.url"
+                           class="relative inline-flex items-center px-2 py-2 text-sm font-semibold"
+                           :class="link.active ? 'z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' : 'text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0 disabled'"
+
+                     >
+                         <div v-if="link.label.includes('Next')">
+                                Next
+                                <ChevronRightIcon class="h-5 w-5 ml-1" aria-hidden="true" />
+                         </div>
+                         <div v-else-if="link.label.includes('Previous')">
+                                <ChevronLeftIcon class="h-5 w-5 mr-1" aria-hidden="true" />
+                                Previous
+                         </div>
+                         <div v-else>
+                             {{ link.label }}
+                         </div>
+                     </Link>
                 </nav>
             </div>
         </div>
     </div>
 </template>
 <script setup>
+import {Link} from '@inertiajs/vue3';
 import {ChevronLeftIcon, ChevronRightIcon} from "@heroicons/vue/20/solid/index.js";
 import {computed} from "vue";
 
