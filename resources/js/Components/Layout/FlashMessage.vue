bdbbc35173ffd0e766575eccc5ed623f57b1e9d5
Rubrasum <josephbetbeze@gmail.com>
2024-06-20 16:56:24 -0400

    Added all the message types to the FlashMessage component.

--- resources/js/Components/Layout/FlashMessage.vue
+++ resources/js/Components/Layout/FlashMessage.vue
@@ -1,23 +1,37 @@
 <template>
-    <div class="rounded-md bg-red-50 p-4">
-        <div class="flex">
-            <div class="flex-shrink-0">
-                <XCircleIcon class="h-5 w-5 text-red-400" aria-hidden="true" />
-            </div>
-            <div class="ml-3">
-                <h3 class="text-sm font-medium text-red-800">{{ message }}</h3>
-            </div>
-        </div>
+    <div v-for="message in messages.success" :key="message.id">
+        <Success :message="message" />
+    </div>
+    <div v-for="message in messages.error" :key="message.id">
+        <Error :message="message" />
+    </div>
+    <div v-for="message in messages.warning" :key="message.id">
+        <Warning :message="message" />
+    </div>
+    <div v-for="message in messages.information" :key="message.id">
+        <Information :message="message" />
     </div>
 </template>
 
 <script setup>
-import { XCircleIcon } from '@heroicons/vue/20/solid';
+import Success from '@/Components/Layout/FlashMessages/Success.vue';
+import Error from '@/Components/Layout/FlashMessages/Error.vue';
+import Warning from '@/Components/Layout/FlashMessages/Warning.vue';
+import Information from '@/Components/Layout/FlashMessages/Information.vue';
+import {computed} from "vue";
+import {usePage} from "@inertiajs/vue3";
 
-const props = defineProps({
-    message: {
-        type: String,
-        default: "",
-    }
-});
+const page = usePage();
+const messages = computed(() => page.props.messages);
+
+// every 20 seconds clear the messages
+setInterval(() => {
+    page.props.messages = {
+        success: [],
+        error: [],
+        warning: [],
+        information: [],
+    };
+}, 20000);
+
 </script>
