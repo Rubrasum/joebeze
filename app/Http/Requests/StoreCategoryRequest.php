d03930f2639873d78df12cce3f125168c1560fe6
Rubrasum <josephbetbeze@gmail.com>
2024-06-27 01:56:31 -0400

    Finished Store Category Request

--- app/Http/Requests/StoreCategoryRequest.php
+++ app/Http/Requests/StoreCategoryRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreCategoryRequest extends FormRequest
@@ -11,18 +12,20 @@ class StoreCategoryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return auth()->check();
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|max:250|unique:categories,name',
+            'slug' => 'required|max:250|unique:categories,slug',
+            'color' => 'required|max:50|unique:categories,color',
         ];
     }
 }
