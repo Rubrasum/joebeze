90a9a1e2807a7a026f49ef75246d05a81a9deadd
Rubrasum <josephbetbeze@gmail.com>
2024-06-27 01:34:10 -0400

    Fixed link error on View Page and confirmed the post forms still working.

--- resources/js/Pages/Admin/Posts/View.vue
+++ resources/js/Pages/Admin/Posts/View.vue
@@ -21,7 +21,7 @@
                                       focus-visible:outline-slate-600 border border-white mr-2`"
 
                                     >Edit</Link>
-                                    <Link :href="`/posts/${post.slug}`"
+                                    <Link :href="`/post/${post.slug}`"
                                           :class="`relative inline-flex items-center rounded-md bg-slate-700 px-3 py-2
                                       text-sm font-semibold text-white shadow-sm hover:bg-slate-600
                                       focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
