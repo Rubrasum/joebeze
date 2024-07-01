6d9ab25b6a12196f635438d806783384df8f81fb
Rubrasum <josephbetbeze@gmail.com>
2024-06-30 23:49:03 -0400

    Added the monthly and yearly kernel command schedule.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,12 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        Commands\BackupFilesDaily::class,
+        Commands\BackupFilesWeekly::class,
+        Commands\BackupFilesMonthly::class,
+        Commands\BackupFilesYearly::class,
+    ];
     /**
      * Define the application's command schedule.
      */
@@ -27,6 +33,22 @@ class Kernel extends ConsoleKernel
             ->environments(['production'])
             ->before(function () {
                 config(['backup.destination.disks.s3.path' => 'Files/Weekly']);
+            });
+
+        $schedule->command('backup:monthly')
+            ->monthlyOn(1)
+            ->at('02:00')
+            ->environments(['production'])
+            ->before(function () {
+                config(['backup.destination.disks.s3.path' => 'Files/Monthly']);
+            });
+
+        $schedule->command('backup:yearly')
+            ->yearlyOn(1, 1)
+            ->at('03:00')
+            ->environments(['production'])
+            ->before(function () {
+                config(['backup.destination.disks.s3.path' => 'Files/Yearly']);
             });
     }
 
