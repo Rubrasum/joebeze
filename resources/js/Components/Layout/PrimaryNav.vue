baae00794750983326f66ab9c8b56c38fad12ace
Rubrasum <josephbetbeze@gmail.com>
2024-07-03 16:20:28 -0400

    Extended space between top and logo, added color to accents, moved filter posts options into primary nav, gonna move them again.

--- resources/js/Components/Layout/PrimaryNav.vue
+++ resources/js/Components/Layout/PrimaryNav.vue
@@ -11,7 +11,7 @@
                 <Popover class="relative">
                     <PopoverButton class="flex items-center gap-x-1 text-sm font-semibold leading-6 text-primary-text">
                         Product
-                        <ChevronDownIcon class="h-5 w-5 flex-none text-secondary-text" aria-hidden="true" />
+                        <ChevronDownIcon class="h-5 w-5 flex-none text-accent" aria-hidden="true" />
                     </PopoverButton>
 
                     <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1"
@@ -57,7 +57,7 @@
                 <Popover class="relative">
                     <PopoverButton class="flex items-center gap-x-1 text-sm font-semibold leading-6 text-primary-text">
                         Company
-                        <ChevronDownIcon class="h-5 w-5 flex-none text-secondary-text" aria-hidden="true" />
+                        <ChevronDownIcon class="h-5 w-5 flex-none text-accent" aria-hidden="true" />
                     </PopoverButton>
 
                     <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
@@ -72,103 +72,104 @@
                         </PopoverPanel>
                     </transition>
                 </Popover>
-            </PopoverGroup>
-            <div class=" px-4 h-full z-[99] flex">
-                <nav class="h-full flex justify-between items-center">
-                    <div id="Logo container placeholder" class="w-full">
-                        <div class="block z-[110] max-w-[33.85vw]"></div>
-                    </div>
-                    <div class="hidden lg:flex items-center">
-                        <div class="relative mr-4" :class="{ hidden : !posts }">
-                            <!-- Categories dropdown component -->
-                            <Dropdown />
+                <div class=" px-4 h-full z-[99] flex">
+                    <nav class="h-full flex justify-between items-center">
+                        <div id="Logo container placeholder" class="w-full">
+                            <div class="block z-[110] max-w-[33.85vw]"></div>
                         </div>
-                        <div>
-                            <form @submit.prevent="handleSearch" ref="form">
-                                <input
-                                    v-if="currentCategory"
-                                    type="hidden"
-                                    name="category"
-                                    :value="currentCategory.name"
-                                />
-                                <input
-                                    type="search"
-                                    name="search"
-                                    placeholder="Search"
-                                    class="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                    v-model="search"
-                                />
-                            </form>
+                        <div class="hidden lg:flex items-center">
+                            <div class="relative mr-4" :class="{ hidden : !posts }">
+                                <!-- Categories dropdown component -->
+                                <Dropdown />
+                            </div>
+                            <div>
+                                <form @submit.prevent="handleSearch" ref="form">
+                                    <input
+                                        v-if="currentCategory"
+                                        type="hidden"
+                                        name="category"
+                                        :value="currentCategory.name"
+                                    />
+                                    <input
+                                        type="search"
+                                        name="search"
+                                        placeholder="Search"
+                                        class="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                        v-model="search"
+                                    />
+                                </form>
+                            </div>
                         </div>
-                    </div>
-                    <div class="lg:hidden">
-                        <button
-                            @click="open = !open"
-                            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white bg-gray-800 border-2 border-gray-300"
-                            aria-controls="mobile-menu"
-                            :aria-expanded="open"
-                        >
-                            <span class="sr-only">Open main menu</span>
-                            <svg
-                                class="block h-6 w-6"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                aria-hidden="true"
+                        <div class="lg:hidden">
+                            <button
+                                @click="open = !open"
+                                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white bg-gray-800 border-2 border-gray-300"
+                                aria-controls="mobile-menu"
+                                :aria-expanded="open"
                             >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M4 6h16M4 12h16M4 18h16"
-                                />
-                            </svg>
-                            <svg
-                                class="hidden h-6 w-6"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M6 18L18 6M6 6l12 12"
-                                />
-                            </svg>
-                        </button>
-                    </div>
-                </nav>
+                                <span class="sr-only">Open main menu</span>
+                                <svg
+                                    class="block h-6 w-6"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    aria-hidden="true"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M4 6h16M4 12h16M4 18h16"
+                                    />
+                                </svg>
+                                <svg
+                                    class="hidden h-6 w-6"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    aria-hidden="true"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M6 18L18 6M6 6l12 12"
+                                    />
+                                </svg>
+                            </button>
+                        </div>
+                    </nav>
 
-                <div v-show="open" class="lg:hidden" id="mobile-menu">
-                    <div class="px-2 pt-2 pb-3 space-y-1">
-                        <div class="absolute lg:relative mb-4 top-[4.25rem] right-[0.3rem]">
-                            <!-- Categories dropdown component -->
-                            <Dropdown />
-                        </div>
-                        <div class="absolute lg:relative top-[6.5rem] right-[1rem]">
-                            <form @submit.prevent="handleSearch">
-                                <input
-                                    v-if="currentCategory"
-                                    type="hidden"
-                                    name="category"
-                                    :value="currentCategory.name"
-                                />
-                                <input
-                                    type="text"
-                                    name="search"
-                                    placeholder="Search"
-                                    class="block w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                    v-model="search"
-                                />
-                            </form>
+                    <div v-show="open" class="lg:hidden" id="mobile-menu">
+                        <div class="px-2 pt-2 pb-3 space-y-1">
+                            <div class="absolute lg:relative mb-4 top-[4.25rem] right-[0.3rem]">
+                                <!-- Categories dropdown component -->
+                                <Dropdown />
+                            </div>
+                            <div class="absolute lg:relative top-[6.5rem] right-[1rem]">
+                                <form @submit.prevent="handleSearch">
+                                    <input
+                                        v-if="currentCategory"
+                                        type="hidden"
+                                        name="category"
+                                        :value="currentCategory.name"
+                                    />
+                                    <input
+                                        type="text"
+                                        name="search"
+                                        placeholder="Search"
+                                        class="block w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                        v-model="search"
+                                    />
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </PopoverGroup>
+
             <div class="hidden lg:flex lg:flex-1 lg:justify-end">
                 <a href="#" class="text-sm font-semibold leading-6 text-primary-text">Log in <span aria-hidden="true">&rarr;</span></a>
             </div>
