c2ac26c50e3bf92a2b4f6045725c725a81c3ae49
Rubrasum <josephbetbeze@gmail.com>
2024-07-03 12:27:56 -0400

    Updated coloring on featured card to go with defined theme.

--- resources/js/Components/Posts/FeaturedCard.vue
+++ resources/js/Components/Posts/FeaturedCard.vue
@@ -1,14 +1,16 @@
 <template>
-    <div class="bg-black">
+    <div class="bg-background">
         <div class="w-full relative">
-            <div class="top-0 left-0 w-full bg-black bg-opacity-60 flex flex-col justify-between border-t-8 border-gray-800">
-                <div class="mb-8 mx-[10vw] pt-8 lg:pt-16 px-4 xl:px-16 pb-8 lg:grid lg:grid-cols-5 lg:gap-5 border-l-8 border-r-2 border-b-2 border-gray-800 rounded-br-md">
-                    <div class="lg:col-span-2 border-gray-800">
-                        <img :src="`/images/category-logos/${post.category.name}-cat-logo.jpg`" alt="Image" class="w-full h-auto border-2 border-gray-800">
+            <div class="top-0 left-0 w-full bg-background bg-opacity-60 flex flex-col justify-between border-t-8 border-primary">
+                <div class="mb-8 mx-[10vw] pt-8 lg:pt-16 px-4 xl:px-16 pb-8 lg:grid lg:grid-cols-5 lg:gap-5 border-l-8
+                            border-r-2 border-b-2 border-primary rounded-br-md">
+                    <div class="lg:col-span-2 border-primary">
+                        <img :src="`/images/category-logos/${post.category.name}-cat-logo.jpg`" alt="Image"
+                             class="w-full h-auto border-2 border-primary">
                     </div>
                     <div class="lg:col-span-3">
                         <Link :href="`/post/${post.slug}`" preserve-state >
-                            <h1 class="text-white text-3xl md:text-4xl font-bold">{{ post.title }}</h1>
+                            <h1 class="text-secondary text-3xl md:text-4xl font-bold">{{ post.title }}</h1>
                         </Link>
                         <div class="mt-4 space-x-2">
                             <Link  :href="`/?category=${post.category.slug}`" preserve-state
@@ -23,13 +25,13 @@
                             <p class="text-white pt-2">Published: <span class="font-semibold">{{ formattedDate }}</span></p>
                             <footer>
                                 <Link :href="`/post/${post.slug}`"  :only="['post']"
-                                      class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded btn-read-gray"
+                                      class="bg-accent-dark hover:bg-accent text-secondary font-bold py-2 px-4 rounded btn-read-gray"
                                       preserve-state
                                 >Read more</Link>
                             </footer>
                         </div>
-                        <div class="lg:col-span-5 border-gray-800 mt-8 text-white text-sm xl:text-base">
-                            <p class="mb-4 text-justify text-white" v-html="post.excerpt"></p>
+                        <div class="lg:col-span-5 border-primary mt-8 text-secondary text-sm xl:text-base">
+                            <p class="mb-4 text-justify text-secondary" v-html="post.excerpt"></p>
                         </div>
                     </div>
                 </div>
