4d2cee8c1720f060f9b35e04ea73bfd6bb0aa3f6
Rubrasum <josephbetbeze@gmail.com>
2024-07-08 14:48:57 -0400

    Added some placeholder content to the portfolio pages.

--- resources/js/Pages/Projects/Anamorphosis.vue
+++ resources/js/Pages/Projects/Anamorphosis.vue
@@ -2,20 +2,21 @@
     <div>
         <main class="bg-black mt-4">
             <article id="post-article"
-                     class="container mx-auto px-16 pb-16 pt-8 my-8 border-x border-secondary-bg
-                     min-w-48 w-3/4 max-w-screen-lg">
+                     class="container mx-auto px-16 pb-16 pt-8 my-8 border-x border-secondary-bg min-w-48 max-w-7xl w-full ">
                 <div class="col-span-3">
-                    <h1 class="text-white text-4xl font-bold mb-10">Page title</h1>
+                    <h1 class="text-white mb-10">Anamorphosis</h1>
                 </div>
 
-                <div class="col-span-2 border-dashed border-y border-secondary-bg text-center flex justify-center items-center space-x-4">
+                <div class="col-span-2 border-dashed border-y border-secondary-bg text-center flex justify-center items-center space-x-4 mb-4">
                     <h6 class="my-2 text-white">
-                        Sub heading
+                        A Powder Physics Engine written in C++ and deployed with WASM.
                     </h6>
                 </div>
 
+                <p>I just like Noita.</p>
+                <br>
+                <p>More information soon...</p>
 
-                <div class="text-white space-y-4 lg:text-lg leading-loose pt-10" >text</div>
             </article>
         </main>
     </div>
--- resources/js/Pages/Projects/MortgaaS.vue
+++ resources/js/Pages/Projects/MortgaaS.vue
@@ -2,20 +2,23 @@
     <div>
         <main class="bg-black mt-4">
             <article id="post-article"
-                     class="container mx-auto px-16 pb-16 pt-8 my-8 border-x border-secondary-bg
-                     min-w-48 w-3/4 max-w-screen-lg">
+                     class="container mx-auto px-16 pb-16 pt-8 my-8 border-x border-secondary-bg min-w-48 max-w-7xl w-full ">
                 <div class="col-span-3">
-                    <h1 class="text-white text-4xl font-bold mb-10">Page title</h1>
+                    <h1 class="text-white mb-10">Mortgaas (This name is being workshopped)</h1>
                 </div>
 
-                <div class="col-span-2 border-dashed border-y border-secondary-bg text-center flex justify-center items-center space-x-4">
+                <div class="col-span-2 border-dashed border-y border-secondary-bg text-center flex justify-center items-center space-x-4 mb-4">
                     <h6 class="my-2 text-white">
-                        Sub heading
+                        Imagine the most modern website
                     </h6>
                 </div>
 
+                <p>The inspiration behind this project is pretty simple and personal. Tarkov developers use the Alpha container as the quintissential selling point for the $140 version of the game. And personally I never wanted to have to pay for that privelege. But to get by without it, you need to manage your stash inventory perfectly. I thought, "wouldn't it be great if I could scan my inventory for quest and items worth selling? Then I could make decisions faster in game, and also learn the items even faster, with an incredible edge on recent changes". At the time, there were no examples of this besides ratscan, which was incredibly unpopular due to the fear of banning. Nowadays a version exists but it is disappointing at best. </p>
+                <br>
+                <p>Currently the version I have developed exists as a python API.</p>
+                <br>
+                <p>More information soon...</p>
 
-                <div class="text-white space-y-4 lg:text-lg leading-loose pt-10" >text</div>
             </article>
         </main>
     </div>
